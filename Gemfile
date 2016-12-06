4100ee173145ae675138d32ccaf91ecbf8790f47
benSacks <sacksomophone@gmail.com>
2016-12-06 16:20:58 -0500

    added amazon s3 for paperclip uploads to heroku

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem "bootstrap-sass"
 gem "devise", "4.2.0"
 gem "rake"
 gem "paperclip", '~> 4.2.0'
+gem "aws-sdk", '~> 2'
 
 group :development, :test do 
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,15 @@ Rails.application.configure do
   # required for Heroku
   # must set this to actual host name
   config.action_mailer.default_url_options = { host: "pinteresting-ii.herokuapp.com" }
+
+  # Sets Paperclip to upload images only to Amazon S3
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
 end
